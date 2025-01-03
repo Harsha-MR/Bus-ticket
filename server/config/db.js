22b4d104729ccf350d3e36ef44e36a68c7b9aacb
Harsha-MR <harshakumarmr88@gmail.com>
2025-01-04 00:52:37 +0530

    mongodb url removed

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async ()=> {
   try {
-    await mongoose.connect('mongodb+srv://harshakumarmr88:CQY4rofR61aZizTv@cluster0.ptnia.mongodb.net/UsersData')
+    await mongoose.connect('')
     console.log("Database is connected"); 
   }
   catch(error) {
