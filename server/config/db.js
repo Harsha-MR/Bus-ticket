8f8d6e5513230eedfaf3a4b586be65fe2a042f0f
Harsha-MR <harshakumarmr88@gmail.com>
2025-01-04 12:16:40 +0530

    userValidation added

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const connectDB = async ()=> {
+  try {
+    await mongoose.connect('mongodb+srv://harshakumarmr88:CQY4rofR61aZizTv@cluster0.ptnia.mongodb.net/UsersData')
+    console.log("Database is connected"); 
+  }
+  catch(error) {
+   console.log(`${error}`)
+  }
+}
+
+export default connectDB;
