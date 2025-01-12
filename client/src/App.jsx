48bc542528dcd44cf9bd8a40371c778cfc85ba35
Harsha-MR <harshakumarmr88@gmail.com>
2025-01-12 22:21:06 +0530

    all header section components are working properly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
-// // src/App.jsx
-// import React, { useState, useEffect } from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import { auth } from './firebase';
-// import { onAuthStateChanged } from 'firebase/auth';
-// import Home from './pages/Home';
-// import SearchResults from './pages/SearchResults';
-// import Booking from './pages/Booking';
-// import Payment from './pages/Payment';
-// import Confirmation from './pages/Confirmation';
-// import SignUp from './pages/SignUp';
-// import Login from './pages/Login';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
+// src/App.jsx
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { auth } from './firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+import Home from './pages/Home';
+import SearchResults from './pages/SearchResults';
+import Booking from './pages/Booking';
+import Payment from './pages/Payment';
+import Confirmation from './pages/Confirmation';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 // const App = () => {
 //   const [user, setUser] = useState(null);
@@ -42,23 +42,27 @@
 
 // export default App;
 
-// src/App.jsx
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';  // Ensure you import Route and Routes
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Header from './components/Header';
-import Footer from './components/Footer';
+// // src/App.jsx
+// import React from 'react';
+// import { Route, Routes } from 'react-router-dom';  // Ensure you import Route and Routes
+// import Home from './pages/Home';
+// import Login from './pages/Login';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div>
       <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          
-        </Routes>
+      <Routes>
+        <Route path="/" element={ <Home />} />
+        <Route path="/search-results" element={<SearchResults /> } />
+        <Route path="/booking" element={ <Booking /> } />
+        <Route path="/payment" element={ <Payment />} />
+        <Route path="/confirmation" element={ <Confirmation /> } />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <Footer />
     </div>
   );
