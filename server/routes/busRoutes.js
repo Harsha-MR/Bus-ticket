3635ec7eb55d33c09f34671c424344130af88d52
Harsha-MR <harshakumarmr88@gmail.com>
2025-01-20 22:15:31 +0530

    get bus by id added

--- server/routes/busRoutes.js
+++ server/routes/busRoutes.js
@@ -2,9 +2,10 @@
 
 import express from "express";
 //import authMiddleware from "../middlewares/authMiddleware.js";
-import { getBusesForRoute } from "../controllers/busController.js";
+import { getBusById, getBusesForRoute } from "../controllers/busController.js";
 import { addBus, getAllBuses, updateBus, deleteBus,addRoute } from "../controllers/busController.js";
 import { authMiddleware, adminMiddleware } from "../middlewares/adminMiddleware.js";
+
 
 const   router = express.Router();
 
@@ -15,6 +16,7 @@ router.get("/",getBusesForRoute);
 // Routes for managing buses
 router.post("/", authMiddleware, adminMiddleware, addBus); // Add a new bus
 router.get("/getBuses", authMiddleware, getAllBuses); // Get all buses
+router.get("/getBuses/:id", authMiddleware, getBusById); // Get bus by ID
 router.put("/:id", authMiddleware, adminMiddleware, updateBus); // Update bus details
 router.delete("/:id", authMiddleware, adminMiddleware, deleteBus); // Delete a bus
 
